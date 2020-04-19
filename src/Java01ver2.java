28fc747586b93520a85a0d2a2d604054faba9ece
sayyes0928 <63114581+sayyes0928@users.noreply.github.com>
2020-04-20 01:55:43 +0900

    java 과제 수정
    1. 성적입력
     - 21#의 if문 조건을 [avg <50] 에서 [ kor < 50 || eng < 50 || mat <50] 으로 수정
    
    2. 구구단 출력의 반복구문을 줄임

--- src/Java01ver2.java
+++ src/Java01ver2.java
@@ -18,8 +18,11 @@ public class Java01ver2 {
 			int total = kor + eng + mat;
 			int avg = total / 3;
 
-			if (avg < 50) {
+			if (kor < 50 || eng < 50 || mat <50) {
 				System.out.println("50점 이상으로 입력하십시오.");
+				 kor = sc.nextInt();
+				 eng = sc.nextInt();
+				 mat = sc.nextInt();
 			} else {
 				System.out.println("입력한 성적은");
 				System.out.println("1.국어 : " + kor);
@@ -28,11 +31,11 @@ public class Java01ver2 {
 				System.out.println("총점은 : " + total);
 				System.out.println("평균은 : " + avg);
 				if (avg >= 90) {
-					System.out.println("A학점");
+					System.out.println("학점은 : A학점");
 				} else if (avg < 90 && avg >= 80) {
-					System.out.println("B학점");
+					System.out.println("학점은 : B학점");
 				} else {
-					System.out.println("F학점");
+					System.out.println("학점은 : F학점");
 				}
 				break;
 			}
--- src/Test02.java
+++ src/Test02.java
@@ -6,28 +6,27 @@ public class Test02 {
 		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 
-		System.out.println("숫자를 입력하세요");
+		System.out.println("첫번째 숫자를 입력하세요");
 		System.out.println("두번째 숫자를 입력하세요");
 
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 
-		if (a < b) {
-			for (; a <= b; a++) {
-				for (int c = 1; c <= 9; c++) {
-					System.out.println(a + "*" + c + "=" + (a * c));
-				}
-			}
-		} else {
-			while (a > b) {
+		while (true) {
+			if (b < a) {
 				System.out.println("두번째 숫자를 더 큰 수로 입력하세요");
 				a = scan.nextInt();
 				b = scan.nextInt();
-			}
-			for (; a <= b; a++) {
-				for (int c = 1; c <= 9; c++) {
-					System.out.println(a + "*" + c + "=" + (a * c));
+			} else {
+				for (; a <= b; a++) {
+					System.out.println("=========");
+					System.out.println(a + "단 시작");
+
+					for (int c = 1; c <= 9; c++) {
+						System.out.println(a + "*" + c + "=" + (a * c));
+					}
 				}
+				break;
 			}
 		}
 	}
