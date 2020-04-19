f5e5847840f0da45071950bb4c887d78ea4956ae
sayyes0928 <63114581+sayyes0928@users.noreply.github.com>
2020-04-19 18:57:04 +0900

    java 성적입력
    1. while 을 사용해 50점 미만 입력시 반복적으로 값을 요청
    2. break 를 걸어 값이 출력되면 종료.

---
+++ src/Java01ver2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Java01ver2 {
+
+	public static void main(String[] args) {
+
+		while (true) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("각 과목의 점수를 입력하세요");
+			System.out.println("1.국어");
+			System.out.println("2.영어");
+			System.out.println("3.수학");
+
+			int kor = sc.nextInt();
+			int eng = sc.nextInt();
+			int mat = sc.nextInt();
+
+			int total = kor + eng + mat;
+			int avg = total / 3;
+
+			if (avg < 50) {
+				System.out.println("50점 이상으로 입력하십시오.");
+			} else {
+				System.out.println("입력한 성적은");
+				System.out.println("1.국어 : " + kor);
+				System.out.println("2.영어 : " + eng);
+				System.out.println("3.수학 : " + mat);
+				System.out.println("총점은 : " + total);
+				System.out.println("평균은 : " + avg);
+				if (avg >= 90) {
+					System.out.println("A학점");
+				} else if (avg < 90 && avg >= 80) {
+					System.out.println("B학점");
+				} else {
+					System.out.println("F학점");
+				}
+				break;
+			}
+		}
+	}
+}
